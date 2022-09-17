c6f1c6ef635700fe1aef3cb854489ed68d79b71b
Devaraj <devarajthadaka33@gmail.com>
2022-09-17 07:47:46 +0530

    posted form

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  user:User = new User();
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void{
+
+  }
+  onSubmit(){
+    console.log(this.user);
+  }
+}
